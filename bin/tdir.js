bc70800e203655ea766eb84c81c8c1fa7ab49c09
Florian Albertz <flo.albertz@gmail.com>
2014-10-09 21:42:58 +0200

    Complete update

--- bin/tdir.js
+++ bin/tdir.js
@@ -12,10 +12,12 @@ program
 	.description('Create a template of the current directory')
 	.option('-t, --template <template-string>', 'set the template detection regex')
 	.option('-o, --output <file>', 'File to write the output to')
+	.option('-e, --encoding <encoding>', 'Encoding to use for the output (default base64)')
 	.action(function(directory, ctx) {
 		tdir.scan({
 			directory: directory,
-			template: ctx.template ? new RegExp(ctx.template) : undefined
+			template: ctx.template ? new RegExp(ctx.template) : undefined,
+			encoding: ctx.encoding ? ctx.encoding : undefined
 		}, function(err, contents) {
 			if(err) throw err;
 			var output = JSON.stringify(contents, 0, 2);
@@ -23,7 +25,7 @@ program
 			if(!ctx.output) {
 				console.log(output);
 			} else {
-				fs.writeFile(options.output, output);
+				fs.writeFile(ctx.output, output);
 			}
 		});
 	});
@@ -31,7 +33,7 @@ program
 program
 	.command('apply <template>')
 	.description('Restore a template. To use file-based templating, use this module programatically')
-	.option('-d, --directory', 'Directory to apply the template to')
+	.option('-d, --directory <directory>', 'Directory to apply the template to')
 	.action(function(template, ctx) {
 		tdir.apply({
 			template: JSON.parse(fs.readFileSync(template, 'utf8')),
